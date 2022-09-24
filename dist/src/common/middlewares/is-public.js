45149eafbde0e7195750528c399d3afc29ab9c0c
Edison Chan <edisonchanwk@gmail.com>
2022-09-24 18:33:38 +0800

    chore: remove files

--- dist/src/common/middlewares/is-public.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isPublic = void 0;
-const isPublic = (req, res, next) => {
-    req.isPublic = true;
-    next();
-};
-exports.isPublic = isPublic;
-//# sourceMappingURL=is-public.js.map
--- dist/src/modules/auth/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authController = void 0;
-var auth_controller_1 = require("./auth.controller");
-Object.defineProperty(exports, "authController", { enumerable: true, get: function () { return auth_controller_1.authController; } });
-//# sourceMappingURL=index.js.map
